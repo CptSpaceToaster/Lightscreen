547577473763ccb7138756fb3d4a507078e8b53d
CptSpaceToaster <Lugia978@yahoo.com>
2015-03-08 09:44:24 -0400

    Starting the GUI

--- src/main/java/Lightscreen.java
+++ src/main/java/Lightscreen.java
@@ -1,6 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class Lightscreen {
+public class Lightscreen extends JFrame {
+    /* GUI Stuff */
+    private JPanel homePanel;
+
+    /**
+     * Main method to be called externally. Creates an instance of
+     * Lightscreen, which starts off this carnival ride!
+     *
+     * @param args Command Line Arguments... currently unused
+     */
     public static void main(String[] args) {
+        new Lightscreen();
+    }
+
+    private Lightscreen() {
+        createElements();
+        addElements();
+
+        // Removes the TitleBar, the X, minimize, and maximize buttons
+        // setUndecorated(true);
+        // getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+
+        // The X doesn't exist any more, but I set the default close operation
+        // anyways...
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Transparent JFrame Background
+        // setBackground(new Color(0,0,0,0));
+
+        // Frame Size
+        setSize(600, 400);
+
+        // Start window in the center of the main screen
+        setLocationRelativeTo(null);
+
+        // We should probably display this thing
+        setVisible(true);
+
         System.out.println("Hello World");
+    }
+
+    /**
+     * Make and configure things
+     */
+    private void createElements() {
+        homePanel = new JPanel();
+        homePanel.setBackground(new Color(28, 28, 28));
+    }
+
+    /**
+     * Put the things in other things
+     */
+    private void addElements() {
+        // Add the Frame's panel to the frame
+        add(homePanel);
+
+
     }
 }
