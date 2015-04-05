abac601f3c95dc702bfc8277f7fa96496b7ee3f0
CptSpaceToaster <CptSpaceToaster@gmail.com>
2015-04-04 23:52:50 -0400

    cleanup

--- src/main/java/Lightscreen.java
+++ src/main/java/Lightscreen.java
@@ -17,7 +17,6 @@ public class Lightscreen extends JFrame {
     }
 
     private Lightscreen() {
-
         createElements();
         addElements();
 
@@ -40,34 +39,9 @@ public class Lightscreen extends JFrame {
         // We should probably display this thing
         setVisible(true);
 
-        System.out.println("Creating Song");
-        Song s = new Song();
-        s.setLyric("A duck walked up to a lemonade stand\n" + "and he said to the man running the stand\n" + "Hey. got any grapes?", "chorus1");
-        s.setLyric("When the duck walked up to the lemonade stand\n" + "And he said to the man that was running the stand\n" + "Hey.  You got any glue?", "chorus2");
-        s.setLyric("The man said no we just sell lemonade.  It’s cold and its fresh and it’s all home made.  Can I sell you glass? The duck said “I’ll pass”.\n"
-                + "Then he waddled away.  Till the very next day.", "verse1");
-        s.setLyric("The man said no, like I said yesterday, we just sell lemonade okay?\n" + "Why not give it a try?   The duck said Goodbye.\n"
-                + "Then he waddled away.  He waddled away.  He waddled away\n" + "Till the very next day.", "verse2");
-        s.setLyric(
-                "The man said look, this is getting old.  Lemonade’s all we’ve ever sold. Why not give it a go?   The duck said “No.”\n" + "Then he waddled away.  He waddled away.  He waddled away\n"
-                        + "Till the very next day.", "verse3");
-        s.setLyric("The man said THAT’S IT!!  If you don’t stay away,duck,  I’ll glue you to a tree and leave you there all day, stuck.\n" + "So don’t get to close!  The duck said Adios.\n"
-                + "Then he waddled away.  He waddled away.  He waddled away\n" + "Till the very next day.", "verse4");
-        s.setLyric("What?\n" + "You got any glue?\n" + "No, why would I– Oh!\n" + "Then one more question for you:\n" + "Got any grapes?", "refrain");
-
-        s.addLyric("verse1", "chorus1", "verse2", "chorus1", "verse3", "chorus1", "verse4", "chorus2", "refrain");
-
-        System.out.println("Saving Song 1");
-        IO.save(s, "song1.txt");
-
-        System.out.println("Loading Song 1 into Song 2");
-        Song song2 = IO.load("song1.txt");
-
-        System.out.println("Adding verse1 to the end");
-        song2.addLyric("verse1");
-
-        System.out.println("Saving Song 2");
-        IO.save(song2, "song2.txt");
+        System.out.println("Loading Song");
+        Song s = IO.load("song1.txt");
+        System.out.println(s.songOrder);
     }
 
     /**
